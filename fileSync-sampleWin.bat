aaee759fa52f842048e956a1eadf37dcfed987c4
oldtrarunner <ultrarunner@sbcglobal.net>
2015-01-19 12:02:37 -0800

    Create fileSync-sampleWin.bat

---
+++ fileSync-sampleWin.bat
@@ -0,0 +1,6 @@
+set oldpath=%cd%
+set classpath=C:\keith\computer\programs\java\fileSync
+cd C:\keith\computer\programs\java
+java fileSync -p -d fileSync F:\DADCAT\C\keith\computer\programs\java\fileSync
+cd %oldpath%
+pause
